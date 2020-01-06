def223325c8a2664cea208e11338f12c10b6e7d7
choeminseob <minsub0922@naver.com>
2020-01-06 17:53:24 +0900

    [UI] Set Main Page
    StackView 를 활용한 탭바 설정

--- KElonTinder/ViewController.swift
+++ KElonTinder/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let stackView: UIStackView = {
+        let redView = UIView()
+        redView.backgroundColor = .red
+        redView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView, yellowView])
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 300)
+        //stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    } ()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addSubViews()
+        activateAutolayouts()
     }
-
-
+    
+    private func addSubViews() {
+        view.addSubview(stackView)
+    }
+    
+    private func activateAutolayouts() {
+        stackView.fillSuperview()
+    }
 }
 
