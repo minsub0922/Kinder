cfba981689f6ed1daf750588e3b1c7bb9cccd083
choeminseob <minsub0922@naver.com>
2020-01-06 16:51:12 +0900

    Initial Commit

---
+++ KElonTinderTests/KElonTinderTests.swift
@@ -0,0 +1,34 @@
+//
+//  KElonTinderTests.swift
+//  KElonTinderTests
+//
+//  Created by 최민섭 on 2020/01/02.
+//  Copyright © 2020 최민섭. All rights reserved.
+//
+
+import XCTest
+@testable import KElonTinder
+
+class KElonTinderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
