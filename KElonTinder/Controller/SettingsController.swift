00261ff6de06bc854056f87d258d0a02b715cb43
choeminseob <minsub0922@naver.com>
2020-01-08 13:21:56 +0900

    Get User Info from Firebase Document with Uid by CurrentUser

--- KElonTinder/Controller/SettingsController.swift
+++ KElonTinder/Controller/SettingsController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import JGProgressHUD
+import SDWebImage
 
 class CustomImagePickerController: UIImagePickerController {
     var imageButton: UIButton?
@@ -45,6 +48,38 @@ class SettingsController: UITableViewController {
         tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
         tableView.tableFooterView = UIView()
         tableView.keyboardDismissMode = .interactive
+        
+        fetchCurrentUser()
+    }
+    
+    var user: User?
+    
+    fileprivate func fetchCurrentUser() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Firestore.firestore().collection("users").document(uid).getDocument { (snapshot, err) in
+            if let err = err {
+                print(err)
+                return
+            }
+            guard let dictionary = snapshot?.data() else { return }
+            self.user = User(dictionary: dictionary)
+            self.loadUserPhotos()
+            
+            self.tableView.reloadData()
+        }
+    }
+    
+    fileprivate func loadUserPhotos() {
+        guard
+            let imageUrl = self.user?.imageUrl1,
+            let url = URL(string: imageUrl)
+            else { return }
+        SDWebImageManager.shared().loadImage(with: url,
+                                             options: .continueInBackground,
+                                             progress: nil) { (image, _, _, _, _, _) in
+                                            self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
     }
     
     lazy var header: UIView = {
@@ -118,10 +153,15 @@ class SettingsController: UITableViewController {
         switch indexPath.section {
         case 1:
             cell.textField.placeholder = "Enter Name"
+            cell.textField.text = user?.name
         case 2:
             cell.textField.placeholder = "Enter Profession"
-        case 1:
+            cell.textField.text = user?.profession
+        case 3:
             cell.textField.placeholder = "Enter Age"
+            if let age = user?.age {
+                cell.textField.text = String(age)
+            }
         default:
             cell.textField.placeholder = "Enter Bio"
         }
