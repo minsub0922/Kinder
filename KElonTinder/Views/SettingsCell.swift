2b41531796d6bcfb2cfe18dcd740dc26780fc4d1
choeminseob <minsub0922@naver.com>
2020-01-08 13:04:18 +0900

    Setup Tableview For Settings

---
+++ KElonTinder/Views/SettingsCell.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsCell.swift
+//  KElonTinder
+//
+//  Created by 최민섭 on 2020/01/08.
+//  Copyright © 2020 최민섭. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    
+    class SettingsTextField: UITextField {
+        
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 44)
+        }
+    }
+    
+    let textField: SettingsTextField = {
+        let tf = SettingsTextField()
+        tf.placeholder = "Enter Name"
+        return tf
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(textField)
+        textField.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
